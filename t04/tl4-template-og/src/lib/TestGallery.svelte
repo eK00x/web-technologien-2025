2a3dd86b2a2b736c0975451cc39a034f78fda827
Alice Alferink <Alicealferink@posteo.de>
2025-07-01 15:15:07 +0200

    tl4 templates ordner upload

---
+++ t04/tl4-template-og/src/lib/TestGallery.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { ratings } from "../stores";
+  
+    
+  </script>
+  
+  <div>
+    <div class="card m-4">
+      <div>
+        {#if $ratings.length > 0}
+          <h3>
+            {$ratings[0].title}
+          </h3>
+          <p>
+            <i class='fa-solid fa-star'></i>
+            <i class='fa-solid fa-star'></i>
+            <i class='fa-solid fa-star'></i>
+            <i class='fa-solid fa-star'></i>
+            <i class='fa-regular fa-star'></i>
+          </p>
+          <p>
+            {$ratings[0].description}
+          <br>
+          <br>
+            {$ratings[0].user}
+          </p>
+        {:else}
+          <h5>There is no rating in the store!</h5>
+        {/if}
+      </div>
+    </div>
+  </div>
+  
