197bd2f9ba05718c9027110825d373aa5da9d855
Alice Alferink <Alicealferink@posteo.de>
2025-07-10 17:31:50 +0200

    comments added to files

--- t04/tl4-template-og/src/lib/Header.svelte
+++ t04/tl4-template-og/src/lib/Header.svelte
@@ -1,11 +1,16 @@
 <script>
-  import logo from '../assets/logo.png';
+  import logo from '../assets/logo.png'; // Festival-Logo importieren
 </script>
 
-<header class="header">
+<header class="header"> <!--Header of the Single-Page-Application-->
   <div class="text-container">
-    <h1>Erfahrungsberichte Rock am See 2025</h1>
-    <h2>Teilt eure Erfahrungen mit uns und den anderen Besucher:innen</h2>
+    <h1>Erfahrungsberichte Rock am See 2025</h1> <!-- Überschrift -->
+    <h2>Teilt eure Erfahrungen mit uns und den anderen Besucher:innen</h2> <!-- Unter-Überschrift -->
   </div>
-  <img src={logo} alt="Festival-Logo" class="logo" />
+
+  <img 
+    src={logo}            
+    alt="Festival-Logo"   
+    class="logo"          
+  />
 </header>
